d472e597093dbd04ebe2937b41d562618a623b26
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-03-18 09:33:25 +0500

    fubction php started

---
+++ func.php
@@ -0,0 +1,6 @@
+<?php 
+	function yuza() {
+		echo "Salom";
+	}
+	yuza();
+ ?>
--- home2.php
+++ home2.php
@@ -1,4 +1,6 @@
-
+<?php 
+$img = array('img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg', 'img/5.jpg', 'img/6.jpg', 'img/7.jpg', 'img/8.jpg');
+ ?>
  <!DOCTYPE html>
  <html>
  <head>
@@ -9,13 +11,23 @@
  </head>
  <body>
  	<h1>Images</h1>
+ 	<table border="1" width="300" style="float: left; margin-right: 20px">
+ 		<th colspan="2">PhP orqali rasm joylash</th>
+ 		<?php for($i=0; $i<8; $i++) { ?>
+ 			<tr>
+ 				<td><img width="300px" height="200px" src="<?php if($i%2==1) { echo $img[$i]; } ?>" alt=""></td>
+ 				<td><img width="300px" height="200px" src="<?php if($i%2==0) { echo $img[$i]; } ?>" alt=""></td>
+ 				
+ 			</tr>
+ 		<?php } ?>
+ 	</table>
  	
  	<table border="1" width="300">
  		<th colspan="2">PhP orqali rasm joylash 2-jadval</th>
  		<?php for($i=1; $i<8; $i++) { ?>
  			<tr>
- 				<td><img width="300px" height="200px" src="img/<?php if($i%2==1) { echo $i; } ?>.jpg"></td>
- 				<td><img width="300px" height="200px" src="img/<?php if($i%2==0) { echo $i; } ?>.jpg"></td>
+ 				<td><img width="300px" height="200px" src="img/<?php if($i%2==1) { echo $i; } ?>.jpg" alt=""></td>
+ 				<td><img width="300px" height="200px" src="img/<?php if($i%2==0) { echo $i; } ?>.jpg" alt=""></td>
  				 				
  			</tr>
  		<?php } ?>
