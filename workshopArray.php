af94f82ba9d9482edd12dbdef8c586b687c8b954
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-03-24 19:18:12 +0500

    foreach funksiyasi yana

--- workshopArray.php
+++ workshopArray.php
@@ -8,12 +8,12 @@ $davlat = array('O`zbekiston','Turkmaniston','Rossiya','Armenia');
 //echo "hisoblash = ", $massiv[3]+$massiv[0];
 //unset($massiv[3]);
 //print_r($massiv)."<br>";
- print_r($davlat);
+//print_r($davlat);
 //sort($davlat);
 //rsort($davlat);
 //print_r($davlat);
 //print_r($massiv1)."<br>";
-$poytaxt = array('Toshkent'=>'O`zbekiston', 'Ashxabod'=>'Turkmaniston', 'Moskva'=>'Rossiya', 'Yerevan'=>'Armenia');
+$poytaxt = array('Toshkent'=>'O`zbekiston', 'Ashxabod'=>'Turkmaniston', 'Moskva'=>'Rossiya', 'Erevan'=>'Armenia');
 
 if(in_array("Turkmaniston", $poytaxt)) {
 	echo "Massivda bu qiymat mavjud";
@@ -29,9 +29,10 @@ if(in_array("Turkmaniston", $poytaxt)) {
 //print_r($poytaxt);
 //shuffle($poytaxt);
 //print_r($poytaxt);
-//$matn = "lorem ipsum icons awesome html code js";
+$matn = "lorem ipsum icons awesome html code js";
 //$a = explode(' ', $matn);
 //print_r($a);
+//echo '<br>';
 //$text = "lorem ipsum icons awesom htm code js";
 //$b = explode('m ', $text);
 //print_r($b);
@@ -41,24 +42,21 @@ if(in_array("Turkmaniston", $poytaxt)) {
 //echo "massiv elementlari yigindisi ", $num[1]+round($nums[2]).'<br>';
 //echo " massiv elementlari ayirmasi ", round($nums[2])-$num[1].'<br>';
 //echo " massiv elementi va alohida o` zgaruvchi ayirmasi ", $num[1]-$c.'<br>';
-/*
-<?
+
+
  $ship = array(
  "Passenger ship" => array("Yacht","Liner","Ferry"),
  "War ship" => array("Battle-wagon","Submarine","Cruiser"),
  "Freight ship" => array("Tank vessel","Dry-cargo ship","Container
  cargo ship")
  );
- foreach($ship as $key => $type)
- {
- echo(
- "<h2>$key</h2>\n"."<ul>\n");
- foreach($type as $ship)
- {
- echo("\t<li>$ship</li>\n");
- }
- }
- echo("</ul>\n");
-?>
-*/
+ foreach($ship as $key => $type) { 
+ 	echo("<h2>$key</h2>"."<ul>"); 	
+ 		foreach($type as $ship) {
+ 			echo("<li>$ship</li>");
+ 		} 
+ 	}
+ echo("</ul>");
+
+
 ?>
