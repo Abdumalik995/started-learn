8797d89991a4f51fa8e229fc0bebf83f6c846279
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-03-25 10:04:21 +0500

    Superglobals cookie

---
+++ Cookie.php
@@ -0,0 +1,19 @@
+<?php 
+	setcookie("nomi", "45", time()+100);
+	setcookie("a", "15", time()+10);
+	setcookie("b", "18", time()+10);
+	setcookie("c", "100", time()+26400);
+	echo "yig'indi= ".$_COOKIE['a']+$_COOKIE['b']+$_COOKIE['c'];
+
+	
+?>
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<?php //echo "vaqtinchalik o`zgaruvchining qiymayi = ".$_COOKIE["nomi"]; ?>
+	</body>
+	</html>	
+ 
