bad34cb0d6dcdec99f3d7f49f1dfb19b83eda427
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-03-26 11:38:19 +0500

    session vazifalar

--- Superglobal.php
+++ Superglobal.php
@@ -2,6 +2,6 @@
 	echo "<pre>";
 	print_r($_SERVER);
 	echo "</pre>";
-	echo $_SERVER[HTTP_HOST];
+	echo $_SERVER['HTTP_HOST'];
 
  ?>
--- natija.php
+++ natija.php
@@ -7,8 +7,11 @@
 
 	if ($javob1 == 12 && $javob2 == 7 && $javob3 == 12) {
 		echo "Javoblar to'g'ri";
-	} else {
-		echo "Javoblarda xatolik bor";
+	} elseif ($javob3 == 12) {
+		echo "faqat savol 3 tug'ri";
+	}
+	 else {
+		echo "Javoblarda xatolik bor ";
 		print_r($_SESSION);
 	}
  ?>
