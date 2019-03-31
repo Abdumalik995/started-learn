c99a9c4f5017f54f8bb3b9f9f13f54a1830ae12a
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-03-31 17:04:12 +0500

    panel php

---
+++ panel.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Control panel</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<!--Header qismining boshlanishi-->
+<header>
+	<img src="img/logo.png">
+		<ul>
+			<li>ADMIN</li>
+		<li><a href=""><img src="img/user.png"></a></li>
+		<li><a href=""><img src="img/search.png"></a></li>
+		<li><a href=""><img src="img/refresh.png"></a></li>
+	</ul>
+		</header>
+<!--header qismining tugashi-->
+<!--chap blokni boshlanishi-->
+<nav>
+	<h1>Menyular bloki</h1>
+	<ul>
+		<li><a href="#">Talabalar</a></li>
+			<li><a href="#">Fakultetlar</a></li>
+			<li><a href="#">Fanlar</a></li>
+			<li><a href="#">Semstr natijalari</a></li>
+			<li><a href="#">Tizimdan chiqish</a></li>
+   
+	</ul>
+</nav>
+<!--chap blokni boshlanishi-->
+<section>
+	<div class="content">
+	<h1>Asosiy ma'lumotlar</h1>
+<table>
+  <tr>
+  	 <th class="small">№</th>
+    <th>Ism Familiyas</th>
+    <th>Manzil</th>
+    <th>Fakultet</th>
+    <th></th>
+     <th></th>
+  </tr>
+  <tr><td>1</td>
+    <td>Axmedov Davron</td>
+    <td>Toshkent shaxri</td>
+    <td>Telekominikatsiya</td>
+    <td class="small"><a href="#"><img src="img/edit.png"></a></td>
+    <td class="small"><a href="#"><img src="img/delete.png"></a></td>
+  </tr>
+  <tr><td>2</td>
+    <td>Axmedov Davron</td>
+    <td>Toshkent shaxri</td>
+    <td>Telekominikatsiya</td>
+    <td class="small"><a href="#"><img src="img/edit.png"></a></td>
+    <td class="small"><a href="#"><img src="img/delete.png"></a></td>
+  </tr>
+  <tr><td>3</td>
+    <td>Axmedov Davron</td>
+    <td>Toshkent shaxri</td>
+    <td>Telekominikatsiya</td>
+    <td class="small"><a href="#"><img src="img/edit.png"></a></td>
+    <td class="small"><a href="#"><img src="img/delete.png"></a></td>
+  </tr>
+  <tr><td>4</td>
+    <td>Axmedov Davron</td>
+    <td>Toshkent shaxri</td>
+    <td>Telekominikatsiya</td>
+    <td class="small"><a href="#"><img src="img/edit.png"></a></td>
+    <td class="small"><a href="#"><img src="img/delete.png"></a></td>
+  </tr>
+  <tr><td>5</td>
+    <td>Axmedov Davron</td>
+    <td>Toshkent shaxri</td>
+    <td>Telekominikatsiya</td>
+    <td class="small"><a href="#"><img src="img/edit.png"></a></td>
+    <td class="small"><a href="#"><img src="img/delete.png"></a></td>
+  </tr>
+  <tr><td>6</td>
+    <td>Axmedov Davron</td>
+    <td>Toshkent shaxri</td>
+    <td>Telekominikatsiya</td>
+    <td class="small"><a href="#"><img src="img/edit.png"></a></td>
+    <td class="small"><a href="#"><img src="img/delete.png"></a></td>
+  </tr>
+  <tr><td>7</td>
+    <td>Axmedov Davron</td>
+    <td>Toshkent shaxri</td>
+    <td>Telekominikatsiya</td>
+    <td class="small"><a href="#"><img src="img/edit.png"></a></td>
+    <td class="small"><a href="#"><img src="img/delete.png"></a></td>
+  </tr>
+  <tr><td>8</td>
+    <td>Axmedov Davron</td>
+    <td>Toshkent shaxri</td>
+    <td>Telekominikatsiya</td>
+    <td class="small"><a href="#"><img src="img/edit.png"></a></td>
+    <td class="small"><a href="#"><img src="img/delete.png"></a></td>
+  </tr>
+  <tr><td>9</td>
+    <td>Axmedov Davron</td>
+    <td>Toshkent shaxri</td>
+    <td>Telekominikatsiya</td>
+    <td class="small"><a href="#"><img src="img/edit.png"></a></td>
+    <td class="small"><a href="#"><img src="img/delete.png"></a></td>
+  </tr> 
+</table>
+	</div>
+</section>
+<!--chap blokni tugashi-->
+<!--footer qismini boshlanishi-->
+<footer>
+Copyright © uicit.uz
+</footer>
+</body>
+</html>
