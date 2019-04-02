0a34f2696037031156d26d125dc0ffc62126dcad
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-04-02 09:46:51 +0500

    admin panel edit php

---
+++ admin/edit.php
@@ -0,0 +1,113 @@
+<?php 
+
+ ?>
+
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>bazap php</title>
+</head>
+<style type="text/css">
+    
+	#mail {
+    position: relative;
+    margin: auto;
+    margin-top: 30px;
+    padding-top: 10px;
+    padding-left: 100px;
+    width: 70%;
+    height: 650px;
+    /*text-align: center;*/
+    background: #fff;
+    box-shadow: 0px 0px 10px #222;
+    overflow: hidden;
+	}
+	#mail>form>input, #mail>form>textarea {
+    margin-bottom: 20px;
+    width: 50%;
+    height: 40px;
+    background: #f2f2f7;
+    font-size: 16px;
+    font-weight: 500;
+    padding-left: 20px;
+    border: none;
+    border-radius: 3px;
+
+	}
+	#mail>form>textarea {
+    padding-top: 7px;
+	}
+	#mail>form>#sub {
+    margin-top: 20px;
+    margin-bottom: 70px;
+    width: 170px;
+    height: 45px;
+    background: crimson;
+    color: #fff;
+    text-align: center;
+    border: none;
+    box-shadow: 0px 0px 10px #7b7474;
+    cursor: pointer;
+	}
+    h1 {
+        text-align: center;
+    }
+    h1 span {
+        color: crimson;
+    }
+    .red {
+        color: red;
+    }
+    input[type="radio"] {
+        width: 20px !important;
+        
+
+    }
+    #comment {
+        margin: 20px 0;
+        border: 1px solid #ccc;
+        padding:25px 10px;
+        border-radius: 5px;
+    }    
+</style>
+<body>
+
+	<div id="mail">
+        <div></div>
+			<h1>Edit <span></span></h1>
+                 
+			<form id="form" action="update.php" method="post" autocomplete="off">
+            <input type="text" name="name"  placeholder="name here">
+            
+            <br>			
+			<input type="text" name="surname" placeholder="surname here">
+            
+            <br>
+            <input type="text" name="adress" placeholder="adress here">
+            
+            <br>
+            <input type="text" name="yunalish">
+            
+            <br><br>
+            
+           
+            <!-- Fakultetni tanlang:<select name="yunalish">
+                <option></option>
+                <option value="1">KIF</option>
+                <option value="2">Telekom</option>
+                <option value="3">Dasturiy</option>
+                <option value="4">Aloqalash</option>
+                <option value="5">Radio va mobil</option>
+            </select> <br> -->
+						
+			<input id="sub" type="submit" name="sub" value="SUBSCRIBE">
+		</form>
+        
+		</div>
+   
+          
+    </div>
+        
+
+</body>
+</html> 
