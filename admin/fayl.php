716921781dbddfb05cd9e659e8bdced4835d0ae6
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-04-04 09:53:43 +0500

    admin panel fayl php + enctype

--- admin/fayl.php
+++ admin/fayl.php
@@ -2,3 +2,17 @@
 //phpinfo(); php versiyalarini nazorat qilish
 
  ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>File php</title>
+ </head>
+ <body>
+ 	
+ 	<form action="upload.php" method="POST" enctype="multipart/form-data">
+ 		
+ 		<input type="file" name="fayl">
+ 		<input type="submit" name="submit" value="Faylni yuklash">
+ 	</form>
+ </body>
+ </html>
