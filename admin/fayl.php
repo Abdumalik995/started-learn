b59a1d60956c49bc1680f645073598e496869b24
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-04-04 10:11:03 +0500

    admin panel fayl php + accept

--- admin/fayl.php
+++ admin/fayl.php
@@ -6,12 +6,13 @@
  <html>
  <head>
  	<title>File php</title>
+
  </head>
  <body>
  	
  	<form action="upload.php" method="POST" enctype="multipart/form-data">
  		
- 		<input type="file" name="fayl">
+ 		<input type="file" name="fayl" accept=".jpg, .jpeg, .png"><br><br>
  		<input type="submit" name="submit" value="Faylni yuklash">
  	</form>
  </body>
