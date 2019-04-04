dec9ef70ef30092cd9f0bb01895ade4ecb881f0a
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-04-04 10:21:10 +0500

    admin panel fayl php fayl upload

--- admin/upload.php
+++ admin/upload.php
@@ -2,4 +2,18 @@
 echo "<pre>";
  print_r($_FILES);
  echo "</pre>";
+// agar forma orqali kelgan fileda xatolik bulmasa
+ if($_FILES['fayl']['error'] == 0) {
+ 	// fayl 
+ 	$faylNomi = $_FILES['fayl']['name'];
+ 	$papke = dirname(_FILE_)."/rasm/".$faylNomi;
+ 	$kesh = $_FILES['fayl']['tmp_name'];
+ 	$a = move_uploaded_file($kesh, $papke);
+
+ 	if ($a == true) {
+ 		echo "Fayl yuklandi: <b>$papke</b>";
+ 	} else {
+ 		echo "Xatolik";
+ 	}
+ }
  ?>
