fec2c7b4b2700b1b6a1d566c9d8f25114b8396aa
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-04-07 08:57:45 +0500

    js fayl navblock uchun

---
+++ admin/blocks/js.php
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+	function res() {
+		let result = confirm("Web saytdan chiqmoqchimisiz");
+		if(result == true) {
+			alert("tashrifingiz uchun rahmat!");
+			location.href = 'index.php';
+		} else {
+			alert('Biz bilan qolganingiz uchun rahmat!');
+		}
+	}
+</script>
--- admin/blocks/navblock.php
+++ admin/blocks/navblock.php
@@ -5,7 +5,7 @@
             <li><a href="panelf.php">Fakultetlar</a></li>
             <li><a href="panelfan.php">Fanlar</a></li>
             <li><a href="panels.php">Semstr natijalari</a></li>
-            <li><a href="index.php" onclick="confirm('web saytdan chiqmoqchimisiz?') ? alert('biz bilan qolganingiz uchun rahmat') : (alert('tashrifingiz uchun rahmat'), location.href)">Tizimdan chiqish</a></li>
+            <li><a href="#" onclick="res()">Tizimdan chiqish</a></li>
        
         </ul>          
 </nav>
