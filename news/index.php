358f30e310f00cba03ceb759e9a2352a2f144e04
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-04-08 22:58:48 +0500

    news img add desk

--- news/index.php
+++ news/index.php
@@ -1,4 +1,21 @@
+<?php 
+  include "db.php";
 
+
+
+
+  $query = "SELECT content.name, content.anons, content.text, content.img, content.datee, content.cat_id, kategoriya.name as ism FROM content LEFT JOIN kategoriya ON content.cat_id=kategoriya.name";
+
+  /*$queryJoin = "SELECT talaba.id AS id, talaba.name AS ISMI, talaba.surname AS Familiyasi, talaba.adress AS Manzili, talaba.img AS Img,  fak.name AS Fakulteti FROM talabalar AS talaba LEFT JOIN yunalish AS fak ON talaba.yunalish_id=fak.id ORDER BY fak.id";*/
+  $result = mysqli_query($db, $query);
+  
+  $contents = array();
+        while($row = mysqli_fetch_assoc($result)) {          
+        $contents[] = $row;
+      }
+     
+print_r($contents);
+ ?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -50,7 +67,9 @@
   <!-- /HEADER ROW -->
 
   
-
+<div>
+  <img src="rasm/<?=$contents[0]['img']?>">
+</div>
 
   <div class="container">
 
